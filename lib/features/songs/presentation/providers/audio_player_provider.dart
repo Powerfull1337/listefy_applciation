340e8107ac1d3529d9f698939b3a669cd26c7c6a
denys <den90021@gmail.com>
2025-06-06 13:29:48 +0300

    moved audio_player_contoller in other folder

--- lib/features/songs/presentation/providers/audio_player_provider.dart
+++ lib/features/songs/presentation/providers/audio_player_provider.dart
@@ -1,7 +1,7 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:listefy_applciation/features/songs/data/models/song_model.dart';
 import 'package:listefy_applciation/features/songs/domain/entities/song.dart';
-import 'package:listefy_applciation/features/songs/presentation/controller/audio_player_controller.dart';
+import 'package:listefy_applciation/shared/providers/audio_player_controller.dart';
 
 final audioPlayerProvider = StateNotifierProvider.autoDispose
     .family<AudioPlayerController, AudioPlayerState, Song>(
